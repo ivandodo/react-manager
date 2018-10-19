e4d164c6ea273251084c4ae167c553f572c52f4f
Ivan Radojevic <ivandodo@gmail.com>
2018-10-19 19:29:24 +0200

    Delete firebase.js

--- firebase.js
+++
@@ -1,8 +0,0 @@
-export default config = {
-    apiKey: "AIzaSyCNkeYmHQhvaouW1NIphWrR-O2OTj59qoM",
-    authDomain: "manager-16dff.firebaseapp.com",
-    databaseURL: "https://manager-16dff.firebaseio.com",
-    projectId: "manager-16dff",
-    storageBucket: "manager-16dff.appspot.com",
-    messagingSenderId: "857234094316"
-};
